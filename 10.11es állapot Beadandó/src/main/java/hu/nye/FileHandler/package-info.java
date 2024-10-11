a7127a3d613d560f46e2f754a3ebe64a3a8e8f0b
Csongor123 <csongi054@gmail.com>
2024-10-11 16:58:29 +0200

    10.11

---
+++ 10.11es állapot Beadandó/src/main/java/hu/nye/FileHandler/package-info.java
@@ -0,0 +1,12 @@
+package hu.nye.FileHandler;
+/*Főbb funkciói:
+
+loadBoardFromFile: Egy adott fájlból beolvassa a játék állását,
+és visszaad egy táblát (két dimenziós karakter tömböt).
+Ha hiba történik a fájl olvasása közben, egy hibaüzenetet jelenít meg.
+saveBoardToFile: A játék aktuális állását menti el egy fájlba,
+soronként írva ki a tábla tartalmát.
+Hiba esetén itt is hibaüzenetet jelenít meg.
+Ez az osztály lehetővé teszi a játék állapotának mentését és visszatöltését,
+ami hasznos lehet, ha a játékot később szeretnénk folytatni.*/
+
